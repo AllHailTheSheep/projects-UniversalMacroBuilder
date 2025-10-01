1e48ce1ac58e6bfb57f97b72dcf98726c7275c1d
Julia Fasick <juli4fasick@gmail.com>
2025-10-01 17:48:04 -0400

    adds main tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,14 @@
-package main.java;
-
-
 import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
         // check arguments. if -output is specified, next arg should be file path and mode is set to record
         // if -input is specified, next arg should be a file and mode is set to replay
+
+        // only one of these strings and files will be populated per run. we could use an enum to help with telling which mode we're in
+        // but we can also just be careful with null checks
+        String out_file_str = null;
+        String in_file_str = null;
         File out_file = null;
         File in_file = null;
 
@@ -14,9 +16,27 @@ public class Main {
             help();
             throw new IllegalArgumentException("ERROR: No arguments given!");
         }
-        for (String arg : args) {
-            if (arg.equals("-output")) {
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("-output")) {
                 System.out.println("in out mode");
+
+                if (i+1 < args.length ) {
+                   out_file_str = args[i+1];
+                } else {
+                    help();
+                    throw new IllegalArgumentException("ERROR: Argument -output requires an argument!");
+                }
+            } else if (args[i].equals("-input")) {
+                System.out.println("in input mode");
+                if (i+1 < args.length ) {
+                    in_file_str = args[i+1];
+                } else  {
+                    help();
+                    throw new IllegalArgumentException("ERROR: Argument -input requires an argument!");
+                }
+            } else {
+                help();
+                throw new IllegalArgumentException("ERROR: Unknown argument: " + args[i]);
             }
         }
 
@@ -24,6 +44,6 @@ public class Main {
     }
 
     private static void help() {
-        System.out.println("Usage: UniversalMacroBuilder.jar (-output <out_path> | -input <in_path>");
+        System.out.println("Usage: UniversalMacroBuilder.jar (-output <out_path> | -input <in_path>)");
     }
 }
